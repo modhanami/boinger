64412d4b4b4f1805cda0b307a64c03a8c7cd6f26
Rooted <modhanami@gmail.com>
2022-07-11 18:20:05 +0700

    Separate routes into their own files

---
+++ endpoints/boing.go
@@ -0,0 +1,80 @@
+package endpoints
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/modhanami/boinger/models"
+	"github.com/modhanami/boinger/services"
+	"net/http"
+	"strconv"
+)
+
+func MakeListEndpoint(s services.BoingService) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		boings, err := s.List()
+		if err != nil {
+			c.JSON(500, ErrorResponseFromError(err))
+			return
+		}
+		c.JSON(200, boings)
+	}
+}
+
+type CreateRequest struct {
+	Text string
+}
+
+func MakeCreateEndpoint(s services.BoingService, u services.UserService) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var request CreateRequest
+		err := c.BindJSON(&request)
+		if err != nil {
+			c.JSON(500, ErrorResponseFromError(err))
+			return
+		}
+
+		rawUserClaims, exists := c.Get(UserClaimsKey)
+		if !exists {
+			c.Status(http.StatusInternalServerError)
+			return
+		}
+
+		userClaims, ok := rawUserClaims.(services.UserClaims)
+		if !ok {
+			c.Status(http.StatusInternalServerError)
+		}
+
+		user, err := u.GetByUid(userClaims.Uid)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, ErrorResponseFromError(err))
+			return
+		}
+
+		err = s.Create(request.Text, user.Id)
+		if err != nil {
+			c.JSON(500, ErrorResponseFromError(err))
+			return
+		}
+		c.Status(201)
+	}
+}
+
+func MakeGetByIdEndpoint(s services.BoingService) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		id, err := strconv.ParseUint(c.Param("id"), 10, 0)
+		if err != nil {
+			c.JSON(500, ErrorResponseFromError(err))
+			return
+		}
+
+		boing, err := s.Get(uint(id))
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, ErrorResponseFromError(err))
+			return
+		} else if boing == (models.BoingModel{}) {
+			c.JSON(http.StatusNotFound, NewErrorResponse("Boing not found"))
+			return
+		}
+
+		c.JSON(http.StatusOK, boing)
+	}
+}
--- endpoints/token.go
+++
@@ -1,9 +0,0 @@
-package endpoints
-
-type UserTokenResponse struct {
-	AccessToken string `json:"accessToken"`
-}
-
-func NewUserTokenResponse(accessToken string) UserTokenResponse {
-	return UserTokenResponse{AccessToken: accessToken}
-}
